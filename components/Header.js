36a67415317f61e4c12c0484738eae65eba3d08c
Alessandro <Alexanderg90@hotmail.it>
2022-06-18 20:43:58 +0200

    header right part done

--- components/Header.js
+++ components/Header.js
@@ -23,9 +23,14 @@ function Header() {
         />
         <SearchIcon className="hidden md:mx-2 md:inline-flex h-8 bg-orange-400 text-white rounded-full p-2 cursor-pointer" />
       </div>
-      {/* RIGHT */}
-          <div>
-              
+      {/* RIGHT - Menu bar*/}
+          <div className="flex space-x-3 items-center justify-end text-gray-500">
+              <p className="hidden md:inline cursor-pointer">Amaca Travel</p>
+              <GlobeAltIcon className="h-6 " />
+              <div className="flex items-center space-x-3 border-2 p-2 rounded-full">
+                  <MenuIcon className="h-6 "/>
+                  <UserCircleIcon className="h-6" />
+              </div>
       </div>
     </header>
   );
