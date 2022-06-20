1649d2d8ca17017e838a8d079bec12934fc75579
Alessandro <Alexanderg90@hotmail.it>
2022-06-20 11:15:47 +0200

    fix

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -8,11 +14,12 @@ function Header() {
       {/* LEFT - Logo*/}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
-          src="/amaca.png"
+          src="/amaca-travel.png"
           layout="fill"
           objectFit="contain"
-          objectPosition="left"
-        />
+                  objectPosition="left"
+                  className="md:hidden" // does it work?
+              />
       </div>
       {/* MIDDLE - Search bar*/}
       <div className="flex items-center md:border-2 md:shadow-sm rounded-full py-2 ">
@@ -24,13 +31,13 @@ function Header() {
         <SearchIcon className="hidden md:mx-2 md:inline-flex h-8 bg-orange-400 text-white rounded-full p-2 cursor-pointer" />
       </div>
       {/* RIGHT - Menu bar*/}
-          <div className="flex space-x-3 items-center justify-end text-gray-500">
-              <p className="hidden md:inline cursor-pointer">Amaca Travel</p>
-              <GlobeAltIcon className="h-6 " />
-              <div className="flex items-center space-x-3 border-2 p-2 rounded-full">
-                  <MenuIcon className="h-6 "/>
-                  <UserCircleIcon className="h-6" />
-              </div>
+      <div className="flex space-x-3 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Amaca Travel</p>
+        <GlobeAltIcon className="h-6 " />
+        <div className="flex items-center space-x-3 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 " />
+          <UserCircleIcon className="h-6" />
+        </div>
       </div>
     </header>
   );
